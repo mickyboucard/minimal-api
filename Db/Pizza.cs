0a4a224de32a3e7cc6b2cea082eea9c901128ebe
Micky Boucard <micaelboucard@gmail.com>
2025-05-09 14:41:51 +1000

    Final routes

---
+++ Db/Pizza.cs
@@ -0,0 +1,7 @@
+namespace MinimalApi.Db;
+
+public record Pizza
+{
+    public int Id { get; set; }
+    public string ? Name { get; set; }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.OpenApi.Models;
+using MinimalApi.Db;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -6,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Minimal API", Description = "A simple minimal API", Version = "v1" });
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Minimal API: Pizza store", Description = "A simple minimal API to support a Pizza store", Version = "v1" });
 });
 
 var app = builder.Build();
@@ -20,6 +21,13 @@ if (app.Environment.IsDevelopment())
     });
 }
 
-app.MapGet("/", () => "Hello World!");
+// Routes
+app.MapGet("/", () => "This is a simple minimal API to support a Pizza store");
+
+app.MapGet("/pizzas/{id}", (int id) => PizzaDB.GetPizza(id));
+app.MapGet("/pizzas", () => PizzaDB.GetPizzas());
+app.MapPost("/pizzas", (Pizza pizza) => PizzaDB.CreatePizza(pizza));
+app.MapPut("/pizzas", (Pizza pizza) => PizzaDB.UpdatePizza(pizza));
+app.MapDelete("/pizzas/{id}", (int id) => PizzaDB.RemovePizza(id));
 
 app.Run();
